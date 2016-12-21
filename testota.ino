82f5a4a42dd75760f1288c25b35e2ac59406fa07
naigolf <niy_golf@hotmail.com>
2016-12-21 16:17:26 +0700

    Add files via upload

---
+++ testota.ino
@@ -0,0 +1,29 @@
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  WiFi.begin("SLINDY4", "074800628");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+  Serial.println("Connected, updating");
+  delay(1000);
+
+  t_httpUpdate_return ret = ESPhttpUpdate.update("https://raw.githubusercontent.com/naigolf/esp8266/master/testota.bin", "", "21 99 13 84 63 72 17 13 B9 ED 0E 8F 00 A5 9B 73 0D D0 56 58");
+  
+  pinMode(16, OUTPUT);
+}
+void loop() {
+
+  digitalWrite(16, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(16, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000); 
+}
